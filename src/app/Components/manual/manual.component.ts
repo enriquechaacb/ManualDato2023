1748694949d35e67f5c74a0b9905daeed6ed501f
enriquechaacb <105406250+enriquechaacb@users.noreply.github.com>
2023-08-13 00:13:45 -0600

    nuevos logos

--- src/app/Components/manual/manual.component.ts
+++ src/app/Components/manual/manual.component.ts
@@ -7,15 +7,23 @@ import { GlobalService } from 'src/app/Services/global.service';
     styleUrls: ['./manual.component.scss']
 })
 export class ManualComponent {
+    public bgrColor: string = "";
     constructor(
         public _globalService: GlobalService
     ){}
     public dimensiones: Array<any> = [
-        { Url: "organizacionypersonas/inicio", Nombre: "Organización y personas", Icono:"people", Clase:"organizacion" },
-        { Url: "transformaciondigital/inicio", Nombre: "Estrategia y transformación digital", Icono:"precision_manufacturing", Clase:"estrategia" },
-        { Url: "datosyanalitica/inicio", Nombre: "Datos y analítica", Icono:"troubleshoot", Clase:"datos" },
-        { Url: "tecnologiasyhabilidadesigitales/inicio", Nombre: "Tecnologías y habilidades digitales", Icono:"memory", Clase:"tecnologias" },
-        { Url: "comunicacion/inicio", Nombre: "Comunicación y canales de venta", Icono:"shopping_cart", Clase:"comunicacion" },
-        { Url: "procesos/inicio", Nombre: "Procesos", Icono:"psychology", Clase:"procesos" }
-    ]
+        { Url: "organizacionypersonas/inicio", Nombre: "Organización y personas", Icono:"organizacion", Clase:"organizacion" },
+        { Url: "transformaciondigital/inicio", Nombre: "Estrategia y transformación digital", Icono:"estrategia", Clase:"estrategia" },
+        { Url: "datosyanalitica/inicio", Nombre: "Datos y analítica", Icono:"datos_analitica", Clase:"datos" },
+        { Url: "tecnologiasyhabilidadesigitales/inicio", Nombre: "Tecnologías y habilidades digitales", Icono:"tecnologias", Clase:"tecnologias" },
+        { Url: "comunicacion/inicio", Nombre: "Comunicación y canales de venta", Icono:"comunicacion", Clase:"comunicacion" },
+        { Url: "procesos/inicio", Nombre: "Procesos", Icono:"procesos", Clase:"procesos" }
+    ];
+    setColorBgr(t:boolean,color:string){
+        if(t){
+            this.bgrColor = color;
+        }else{
+            this.bgrColor = "";
+        }
+    }
 }
